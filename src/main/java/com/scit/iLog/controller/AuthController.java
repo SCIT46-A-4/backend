3c444122250c1728f3e2132340fc2a995e2ea82c
Junseong <bws4115@naver.com>
2025-02-03 12:35:12 +0900

    로그인 페이지 기본 이동 구현, url: /user/login

--- src/main/java/com/scit/iLog/controller/AuthController.java
+++ src/main/java/com/scit/iLog/controller/AuthController.java
@@ -1,4 +1,17 @@
 package com.scit.iLog.controller;
 
-public class AuthController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class AuthController 
+{
+	@GetMapping("/user/login")
+	public String getLoginPage()
+	{
+		return "/auth/signin";
+	}
 }
