d3ac027d2300e9b316d57c722b0e73eda3857b96
eunjinkim <ckatkfkd725@gmail.com>
2025-02-04 12:12:26 +0900

    아이 일기장 목록 페이지 : 아이 일기장 목록으로 넘어가는 페이지 요청
    우리 아이 일기장 목록 페이지로 넘어가는 컨트롤러 메서드 추가,
    diaries 페이지 내용 간단히 적었음(상세페이지 구현은 나중에),
    Security에서 /children/diaries 경로 추가했습니다.

--- src/main/java/com/scit/iLog/config/SecurityConfig.java
+++ src/main/java/com/scit/iLog/config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
                                 "/user/idCheck",
                                 "/user/login",
                                 "/board/boardList",
+                                "/children/diaries",
                                 "/board/boardDetail",
                                 "/board/download",
                                 "/reply/replyInsert",
--- src/main/java/com/scit/iLog/controller/DiaryController.java
+++ src/main/java/com/scit/iLog/controller/DiaryController.java
@@ -1,4 +1,18 @@
 package com.scit.iLog.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/children")
+@Slf4j
 public class DiaryController {
+
+    //일기장 목록 페이지 요청
+    @GetMapping("/diaries")
+    public String selectAll(){
+        return "/children/diaries";
+    }
 }
