17b0bd39acc624878e53100471668d7f21ef41f6
yumyumhwan <soog4039@gmail.com>
2025-02-04 11:17:11 +0900

    statisticsdetails : 통계 상세 페이지메서드 구현
    dashboard와 details에서 childrenController를 통해 맵핑해서
    statisticsDetails.html을 새로 만들어서 연결했습니다.

--- src/main/java/com/scit/iLog/config/SecurityConfig.java
+++ src/main/java/com/scit/iLog/config/SecurityConfig.java
@@ -21,6 +21,7 @@ public class SecurityConfig {
                 auth
                         .requestMatchers(
                                 "/",
+                                "/statisticsDetails",
                                 "/member/join",
                                 "/guides",
                                 "/customerCenter",
--- src/main/java/com/scit/iLog/controller/ChildrenController.java
+++ src/main/java/com/scit/iLog/controller/ChildrenController.java
@@ -1,4 +1,14 @@
 package com.scit.iLog.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ChildrenController {
+	
+	@GetMapping("/statisticsDetails")
+    public String statisticsDetails() {
+        return "/children/statisticsDetails";
+    }
+	
 }
