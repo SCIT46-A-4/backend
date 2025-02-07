207aebe4197114cc344edbd3dd0f86fc816d7a72
Your Name <bokyung99817@gmail.com>
2025-02-07 09:55:53 +0900

    Apply stashed changes to ChildrenController

--- src/main/java/com/scit/iLog/controller/ChildrenController.java
+++ src/main/java/com/scit/iLog/controller/ChildrenController.java
@@ -15,20 +15,40 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/children")
 public class ChildrenController {
+	
 	/* 2025-02-06 ChildService 선언 */
+	/*
+	 * 컨트롤러에서 서비스를 사용할 준비를 하는 코드
+	 * @RequiredArgsConstructor를 통해 자동으로 생성자가 생성
+	 * InfoDetailsService 클래스 주입, 실제 사용할 객체 이름 childService
+	 */
 	private final InfoDetailsService childService;
 
+	
+	
+	/**
+	 * "우리 아이 일기 감정 분석 그래프" 페이지를 보여주는 뷰를 반환하는 핸들러
+	 * @return children/statistics/detailsView.html 뷰 페이지
+	 */
 	@GetMapping("/statisticsDetails")
-    public String statisticsDetails() {
+    public String handleGetStatisticsDetailsView() {
         return "/children/statistics/detailsView";
     }
 
-	// 25/2/5 일기 상세페이지
+	
+	
+	/**
+     * "일기 상세" 페이지를 보여주는 뷰를 반환하는 핸들러
+     * @return children/diaries/diaryDetailsView.html 뷰 페이지
+     */
 	@GetMapping("/diaryDetails")
-	public String getDiaryDetails() {
-		return "/children/diaryDetailsView";
+	public String handleGetDiaryDetailsView() {
+		return "/children/diaries/diaryDetailsView";
 	}
 
+	
+	
+	
 	// 25/2/5 내 아이 상세페이지
 	@GetMapping("/details")
 	public String getChildrenDetails() {
@@ -42,6 +62,7 @@ public class ChildrenController {
 	 * "/children/infoDetails"; }
 	 */
 
+	
 	/* infoDetails에 아동 정보 조회 */
 	@GetMapping("/infoDetails")
 	public String infoDetails(@RequestParam(name="id") Long id, Model model) {
