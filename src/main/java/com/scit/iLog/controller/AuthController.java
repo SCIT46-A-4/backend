b55163534fc9d673ad272c5da13cf56f20d3db93
Junseong <bws4115@naver.com>
2025-03-11 09:38:15 +0900

    error 일으키는 오탈자 수정

--- src/main/java/com/scit/iLog/controller/AuthController.java
+++ src/main/java/com/scit/iLog/controller/AuthController.java
@@ -4,7 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-:import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.scit.iLog.dto.auth.SignUpDTO;
 import com.scit.iLog.service.MemberService;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
